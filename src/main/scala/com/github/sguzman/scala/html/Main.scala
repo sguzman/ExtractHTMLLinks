642230fb4d6ef6b6939130f094ec520d60720aed
Salvador Guzman <guzmansalv@gmail.com>
2018-03-05 14:29:18 -0800

    working solution

--- src/main/scala/com/github/sguzman/scala/html/Main.scala
+++ src/main/scala/com/github/sguzman/scala/html/Main.scala
@@ -1,5 +1,24 @@
 package com.github.sguzman.scala.html
 
+import java.net.URL
+
+import net.ruippeixotog.scalascraper.browser.JsoupBrowser
+import net.ruippeixotog.scalascraper.dsl.DSL.Extract._
+import net.ruippeixotog.scalascraper.dsl.DSL._
+
+import scalaj.http.Http
+
+
 object Main {
-  def main(args: Array[String]): Unit = println("Hello")
+  def url(args: Array[String]) =
+    Config().parse(args, init = CmdArgs()).get.url
+
+  def main(args: Array[String]): Unit =
+    JsoupBrowser()
+      .parseString(
+        Http(new URL(url(args))
+            .toString)
+          .asString.body)
+      .>>(elementList("""a[href]"""))
+      .map(_.attr("href")) foreach println
 }

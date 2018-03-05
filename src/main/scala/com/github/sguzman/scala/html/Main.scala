9a8c7b0dd7ea5454344b7f3e6b733182b304b6df
Salvador Guzman <guzmansalv@gmail.com>
2018-03-05 14:54:29 -0800

    handle # and paths references

--- src/main/scala/com/github/sguzman/scala/html/Main.scala
+++ src/main/scala/com/github/sguzman/scala/html/Main.scala
@@ -11,14 +11,24 @@ import scalaj.http.Http
 
 object Main {
   def url(args: Array[String]) =
-    Config().parse(args, init = CmdArgs()).get.url
+    Config().parse(args, init = CmdArgs()) match {
+      case Some(v) => v.url
+      case None => throw new Exception()
+    }
+
+
+  def port(u: URL) = if (u.getPort == -1) "" else ":" ++ u.getPort.toString
+  def cons(u: URL) = s"${u.getProtocol}://${u.getAuthority}${port(u)}"
+  def consp(u: URL) = s"${u.getProtocol}://${u.getAuthority}${port(u)}${u.getPath}"
+
+  def scheme(u: URL) = JsoupBrowser()
+    .parseString(
+      Http(u.toString).asString.body)
+    .>>(elementList("""a[href]"""))
+    .map(_.attr("href"))
+    .map(t => if (t.startsWith("/")) cons(u) ++ t else t)
+    .map(t => if (t.startsWith("#")) consp(u) else t)
 
   def main(args: Array[String]): Unit =
-    JsoupBrowser()
-      .parseString(
-        Http(new URL(url(args))
-            .toString)
-          .asString.body)
-      .>>(elementList("""a[href]"""))
-      .map(_.attr("href")) foreach println
+    scheme(new URL(url(args))) foreach println
 }
